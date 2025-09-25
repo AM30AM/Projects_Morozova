8bf9e475dd45d1cc4232771e3905fa70316c74c0
AM30AM <arina_mandarinka01@mail.ru>
2025-09-25 17:48:09 +0300

    1

--- PZ_2/PZ_2_1.py
+++ PZ_2/PZ_2_1.py
@@ -1,12 +1,15 @@
 #Дано двухзанчное число. Найдите сумму и произведение его цифр
 
-number = int(input('Введите двухзначное число: '))
+try:
+    number = int(input('Введите двухзначное число: '))
 
-first_n = number // 10
-second_n = number % 10
+    first_n = number // 10
+    second_n = number % 10
 
-summa_number = first_n + second_n
-proisv = first_n * second_n
+    summa_number = first_n + second_n
+    proisv = first_n * second_n
 
-print('Сумма цифр числа: ', summa_number)
-print('Произведение цифр числа: ', proisv)
+    print('Сумма цифр числа: ', summa_number)
+    print('Произведение цифр числа: ', proisv)
+except ValueError:
+    print('Что-то пошло не так')
