59acb782cc7a241b03248dcf5896106e88a90654
AM30AM <arina_mandarinka01@mail.ru>
2026-05-29 21:32:28 +0300

    6

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,6 +1,13 @@
 #Дан список размера N. Найти два соседних элемента, сумма которых максимальна,
 #и вывести эти элементы в порядке возрастания их индексов.
 
+import random
+
+a = []
+for _ in range(10):
+    random_int = random.randint(1, 100)
+    a.append(random_int)
+
 def my_list(lst):
     n = len(lst)
 
@@ -9,13 +16,12 @@ def my_list(lst):
     res = (lst[0], lst[1])
 
     for i in range(1, n):
-        sum = lst[i-1] + lst[i]
-        if sum > max_sum:
-            max_sum = sum
+        suma = lst[i-1] + lst[i]
+        if suma > max_sum:
+            max_sum = suma
             res = lst[i-1], lst[i]
 
     return res
 
-a = [2, 3, 4, 5, 1]
 print('Список: ', a)
 print('Максимальная сумма елементов:', my_list(a))
--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -2,6 +2,13 @@
 #списка вправо на K позиций (при этом A1 перейдет в AK+1, A2 — в AK+2, ..AN-K — в
 #AN, а исходное значение K последних элементов будет потеряно). Первые K
 #элементов полученного списка положить равными 0.
+import random
+
+a = []
+for i in range(10):
+    random_int = random.randint(1, 100)
+    a.append(random_int)
+
 
 def my_list(lst, k):
     n = len(lst)
@@ -9,6 +16,5 @@ def my_list(lst, k):
     res = [0] * k + lst[:n - k]
     return res
 
-a = [2, 3, 4, 5, 1]
-print(a)
-print(my_list(a,1))
+print('Список', a)
+print('Список со сдвигом',my_list(a,3))
